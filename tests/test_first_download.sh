bcdd00b220a3ed88ad09ff40856c05e4ac3e65c3
Yifeng Qiu <yifeng.q@gmail.com>
2023-10-06 22:19:38 -0700

    Refactored tests into test_first_download.sh

--- tests/test_first_download.sh
+++ tests/test_first_download.sh
@@ -1,3 +1,15 @@
 #!/bin/sh
+streamsaver&
+
+if [ -e "/media/download/youtube.com/jawed//NA-Me at the zoo.mp4" ]
+then
+    rm "/media/download/youtube.com/jawed//NA-Me at the zoo.mp4"
+fi
+
+curl -iX POST http://localhost:1718/new -F "url=https://www.youtube.com/watch?v=jNQXAC9IVRw"
+
+sleep 5
 
 if [ -e "/media/download/youtube.com/jawed//NA-Me at the zoo.mp4" ]; then echo "success" ; fi
+
+
